9c9892e5ea5de1d477efdc0e588d29cf34f97190
hoshi-hiyouga <467089858@qq.com>
2017-11-30 19:29:44 +0800

    DP

--- homework/up.c
+++ homework/up.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-int m = 0;
-
-void ups(char * s, char c, int i, int n)
-{
-	if(s[i] < c){
-		if(n > m) m = n;
-	}else{
-		int j;
-		while(s[i] == s[i+1]){
-			i++, n++;
-		}
-		for(j = i+1; s[j] != '\0'; j++){
-			ups(s, s[i], j, n+1);
-		}
-	}
-}
-
 int main()
 {
-	int n = 0;
+	int i, j, k, a[10005][27];
 	char s[10005];
 	fgets(s, 10005, stdin);
-	//puts(s);
-	ups(s, 'a', 0, n);
-	printf("%d\n", m);
+	for(i = 0; i <= 26; i++){
+		a[0][i] = 0;//init
+	}
+	for(i = 1; s[i] != '\0'; i++){
+		a[i][0] = 0;//init
+		k = s[i-1] - 'a' + 1;//char:1 to 26
+		a[i][k] = a[i-1][k] + 1;
+		for(j = 0; j < k; j++){
+			a[i][j] = a[i-1][j];
+		}
+		for(j = k+1; j <= 26; j++){
+			if(a[i][k] > a[i-1][j]){
+				a[i][j] = a[i][k];
+			}else{
+				a[i][j] = a[i-1][j];
+			}
+		}
+	}
+	printf("%d\n", a[i-1][26]);
 	return 0;
 }
